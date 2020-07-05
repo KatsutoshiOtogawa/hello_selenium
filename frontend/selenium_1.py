d77024aa5788942a612104b36da3850c17d6f6f0
KatsutoshiOtogawa <gh1ab7im@gmail.com>
2020-07-05 20:14:36 +0900

    update

--- frontend/selenium_1.py
+++ frontend/selenium_1.py
@@ -6,7 +6,7 @@ import time
 chrome_options = Options()
 chrome_options.add_argument('--headless')
 
-driver = webdriver.Chrome("/Volumes/EXTERNAL_HDD/source/VM/hello_selenium/frontend",chrome_options)
+driver = webdriver.Chrome(chrome_options=chrome_options)
 time.sleep(30000)
 driver.get("https://www.google.com/?hl=ja")
 
